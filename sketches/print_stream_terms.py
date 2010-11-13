41f416e8d2d445b9f058914c49ead5665845b925
Rob Myers <rob@robmyers.org>
2010-11-13 15:19:36 +0000

    Improved display, shared utility code factored out, art_is starting to work.

--- sketches/print_stream_terms.py
+++ sketches/print_stream_terms.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
-import os
-import simplejson
 import tweetstream
+import utilities
 
 # The save file. Must be a full or fully resolvable path
-SAVE_FILE = "./print_stream_terms.save"
+SAVE_FILE = "./cached_print_stream_terms.json"
 
 # The category terms we search for and count
 COLOURS = ["red", "yellow", "blue"]
@@ -20,27 +19,7 @@ COUNT_DEFAULT = 10
 
 #TODO Can we hide the password?
 username = str(raw_input("Twitter username:"))
-password = str(raw_input("Twitter password:"))
-
-def terms_in(terms, tweet_text):
-    """Return a list of the terms the tweet text contains"""
-    contains = []
-    for term in terms:
-        if term in tweet_text.lower():
-            contains.append(term)
-    return contains
-
-def increase_counts(counts, keys):
-    """Increment the counts"""
-    for key in keys:
-        counts[key] = counts.get(key, 0) + 1
-
-def create_counts(keys, default):
-    """Create the default counts"""
-    counts = {}
-    for key in keys:
-        counts[key] = default
-    return counts
+password = utilities.getpass("Twitter password:")
 
 def category_total_count(counts, category_keys):
     """Get the total of all the counts for all the keys in the category"""
@@ -60,34 +39,28 @@ def report_category_counts(counts, categories):
         print
     print
 
-def serialise_counts(terms):
-    temp_filename = SAVE_FILE + "tmp"
-    temp_file = open(temp_filename, 'w')
-    simplejson.dump(terms, temp_file)
-    temp_file.close()
-    os.rename(temp_filename, SAVE_FILE)
-    
-def deserialise_counts():
-    """Deserialize the saved state file. (Dies if it's not there)"""
-    dump_file = open(SAVE_FILE)
-    return simplejson.load(dump_file)
-    
 def go():
     try:
-        counts = deserialise_counts()
+        counts = utilities.deserialise_counts(SAVE_FILE)
     except:
-        counts = create_counts(TERMS, COUNT_DEFAULT)
+        counts = utilities.create_counts(TERMS, COUNT_DEFAULT)
+    tty = utilities.TweetCurses()
     stream = tweetstream.TrackStream(username, password, TERMS)
     for tweet in stream:
         tweet_text = tweet["text"]
-        terms_in_tweet = terms_in(TERMS, tweet_text)
+        terms_in_tweet = utilities.terms_in(TERMS, tweet_text)
         # If we didn't get a spuriously reported tweet...
         if terms_in_tweet:
-            increase_counts(counts, terms_in_tweet)
-            serialise_counts(counts)
-            print "Tweet: %s" % (tweet_text)
+            tty.clear()
+            # Print anything else with the tweet text? Pass it to pad, too...
+            print tweet_text
+            tty.pad_tweet_rows(tweet_text)
+            utilities.increase_counts(counts, terms_in_tweet)
+            utilities.serialise_counts(SAVE_FILE, counts)
             print "Terms: %s" % (" ".join(terms_in_tweet))
             report_category_counts(counts, CATEGORIES)
-        else:
-            print "SPURIOUS TWEET: %s" % (tweet_text)
+        # Otherwise, ignore......
+        #else:
+        #    print "SPURIOUS"
+
 go()
